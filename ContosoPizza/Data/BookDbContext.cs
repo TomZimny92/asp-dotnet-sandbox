de139c89a887d58a5f452023a94a214e08c45f18
Tom Zimny <64618507+TomZimny92@users.noreply.github.com>
2025-10-13 15:04:20 -0400

    starting sql integration

---
+++ ContosoPizza/Data/BookDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ContosoPizza.Models;
+
+namespace ContosoPizza.Data
+{
+    public class BookDbContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options) =>
+            options.UseSqlite(@"Data Source={path}"); // the location of your .db file
+    }
+}
--- ContosoPizza/Services/BookService.cs
+++ ContosoPizza/Services/BookService.cs
@@ -1,7 +1,59 @@
-﻿namespace ContosoPizza.Services
+﻿
+
+using ContosoPizza.Models;
+
+namespace ContosoPizza.Services
 {
     public class BookService
     {
+        static List<Book> Books { get; }
+        static int lastId = 0;
+        static BookService()
+        {
+            GetBooks();
+        }
 
+        private static void GetBooks()
+        {
+            // make call to db to retrieve books
+            Book newBook = new()
+            {
+                Id = lastId++,
+                Title = "testTitle",
+                Rating = 3,
+                Review = "pretty good",
+            };
+            Books.Add(newBook);
+        }
+
+        public static List<Book> GetAll() => Books;
+
+        public static Book? Get(int id) => Books.FirstOrDefault(b => b.Id == id);
+
+        public static void Add(Book book)
+        {
+            book.Id = lastId++;
+            Books.Add(book);
+        }
+
+        public static void Delete(int id)
+        {
+            var book = Get(id);
+            if (book is null)
+            {
+                return;
+            }
+            Books.Remove(book);
+        }
+
+        public static void Update(Book toBeUpdated)
+        {
+            var bookIndex = Books.FindIndex(b => b.Id == toBeUpdated.Id);
+            if (bookIndex == -1)
+            {
+                return;
+            }
+            Books[bookIndex] = toBeUpdated;
+        }
     }
 }
