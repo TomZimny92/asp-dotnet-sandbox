883f9e4f7b5a7d3435a35549535863d03c2de42f
Tom Zimny <64618507+TomZimny92@users.noreply.github.com>
2025-10-23 19:52:28 -0400

    front end and services

---
+++ BlazorApp/Services/BookService.cs
@@ -0,0 +1,29 @@
+﻿using ContosoPizza.Models;
+
+namespace BlazorApp.Services
+{
+    public class BookService
+    {
+        public static async List<Book> GetAll()
+        {
+            // open the database
+            // query the database
+            // format the results
+            // return list of books
+        }
+
+        public static async Book? GetBook(int id)
+        {
+            // open the database
+            // query the database using the id
+            // format the result
+            // if no result, throw exception?
+            // otherwise, return the result
+        }
+
+        public static void AddBook(Book book)
+        {
+
+        }
+    }
+}
